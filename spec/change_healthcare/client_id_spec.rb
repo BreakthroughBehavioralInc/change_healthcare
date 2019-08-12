47024b35090d98f17ecbb1cf192b9fa68f9596f2
Saul Moncada <smoncada@mdlive.com>
2019-08-12 14:07:41 -0400

    Add client_id search_orglab

---
+++ spec/change_healthcare/client_id_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe ChangeHealthcare::ClientId do
+  describe ".search_orglab" do
+    it " Retrieve list of client numbers to be used for the site/lab combination." do
+      resp = ChangeHealthcare::ClientId.search_orglab({lab: "23402537", provider: "23402521", is_active: "Y"})
+
+      expect(resp.first).to be_a_kind_of(ChangeHealthcare::ClientId)
+      expect(resp.first.clientid).not_to be_nil
+      expect(resp.first.id_value).not_to be_nil
+    end
+  end
+end
