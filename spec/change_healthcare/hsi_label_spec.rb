f317dfe934eb0828ae82dc93ad7ceb0715511c4e
Saul Moncada <smoncada@mdlive.com>
2019-08-16 12:30:12 -0400

    Add hsilabel search (needed to create patient)

---
+++ spec/change_healthcare/hsi_label_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe ChangeHealthcare::HsiLabel do
+  describe ".search" do
+    it "Returns the list of existing hsi labels based on the parameters" do
+      resp = ChangeHealthcare::HsiLabel.search({is_hsi_for: "PATIENT", registration: "Y"})
+      expect(resp.first).to be_a_kind_of(ChangeHealthcare::HsiLabel)
+      expect(resp.first.hsilabel).not_to be_nil
+    end
+  end
+end
