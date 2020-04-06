a23ee94b312678b38ab55201b686d4772adbd591
Saul Moncada <smoncada@mdlive.com>
2020-04-06 10:57:48 -0400

    Rails 6 upgrade

--- change_healthcare.gemspec
+++ change_healthcare.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency    "rest-client", "~> 2"
   spec.add_runtime_dependency    "nokogiri", '~> 1'
-  spec.add_runtime_dependency    "activesupport", '~> 5'
+  spec.add_runtime_dependency    "activesupport", '~> 6'
 
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
--- spec/change_healthcare/order_spec.rb
+++ spec/change_healthcare/order_spec.rb
@@ -13,9 +13,10 @@ RSpec.describe ChangeHealthcare::Order do
   describe ".search_by_order_info" do
     it "Retrieves the next requisition number for the lab" do
       mdl_config
-      resp = ChangeHealthcare::Order.search_by_order_info({ placer_order_number: "76047", orderingorganization: "3004115375" })
+      resp = ChangeHealthcare::Order.search_by_order_info({orderingorganization: "3004115375" })
 
       expect(resp.first).to be_a_kind_of(ChangeHealthcare::Order)
+
       expect(resp.first.first_name).not_to be_nil
     end
   end
