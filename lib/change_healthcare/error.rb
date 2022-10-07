9c2c728f690ea24923c8697d2d96e7301e611a67
Saul Moncada <smoncada@mdlive.com>
2022-10-07 19:59:07 -0400

    Avoid error when error does not have description

--- lib/change_healthcare/error.rb
+++ lib/change_healthcare/error.rb
@@ -4,6 +4,8 @@ class ChangeHealthcare::Error < StandardError
   def initialize(xml_root_node)
     @root = xml_root_node
     @code = xml_root_node.children[0].children[0].to_s
-    @description = xml_root_node.children[1].children[0].to_s
+
+    first_children = xml_root_node.children[1]
+    @description = first_children.present? ? first_children.children[0].to_s : ""
   end
 end
--- lib/change_healthcare/version.rb
+++ lib/change_healthcare/version.rb
@@ -1,3 +1,3 @@
 module ChangeHealthcare
-  VERSION = '1.0.3'.freeze
+  VERSION = '1.0.4'.freeze
 end
